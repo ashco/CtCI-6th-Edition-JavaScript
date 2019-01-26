1e94133c38994050956f694d30721809bb5f7c5a
Ashton Christie <achristie@corp.validar.com>
2019-01-25 16:57:32 -0800

    1.3

--- attempts/chapter01/1.2 - Check Perm/checkPermute.js
+++ attempts/chapter01/1.2 - Check Perm/checkPermute.js
@@ -2,4 +2,17 @@
 
 function checkPermute(str1, str2) {
   if (str1.length !== str2.length) return false;
+
+  const sortedStr1 = str1
+    .split('')
+    .sort()
+    .join('');
+  const sortedStr2 = str2
+    .split('')
+    .sort()
+    .join('');
+  return sortedStr1 === sortedStr2;
 }
+
+console.log(checkPermute('ashgotyoback', 'ashrunsawayscared'));
+console.log(checkPermute('j;lkdsaf', 'asfdjkl;'));
---
+++ attempts/chapter01/1.3 - URLify/urlify.js
@@ -0,0 +1,5 @@
+// Write a method to replace all spaces in a string with %20. You may assume that the string has sufficient space at the end to hold the additional chars, and that you are given the true length of the string.
+
+function urlify(string) {}
+
+console.log(urlify('Mr John Smith'), 'Mr%20John%20Smith');
